4ca4611bc3bcdc4f6fd7d54acb0e4c750f2725c0
Tanakh-a <84029154+Tanakh-a@users.noreply.github.com>
2021-10-17 11:16:28 +0600

    Delete write_xlsx.py

--- write_xlsx.py
+++
@@ -1,8 +0,0 @@
-from openpyxl import Workbook
-wb = Workbook()
-ws = wb.active
-#ws['A1'] = "Name"
-testdata =[["Name","Contact Number"],["Sabbir","01714324598"],["Sunve","+880 1677-196458"],["Prionty","0184234587"]]
-for data in testdata:
-    ws.append(data)
-wb.save("excel.xlsx")
